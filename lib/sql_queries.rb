ede51f94c0246c1b147cf6358148bb7a56b58ce2
YanC Lin <yonalin1@gmail.com>
2018-02-06 19:18:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears WHERE color = 'dark brown';"
+  "SELECT color,COUNT(color) FROM bears GROUP BY color ORDER BY count(color)DESC LIMIT 1;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -29,4 +29,3 @@ end
 def selects_bear_that_killed_Tim
   "SELECT * FROM bears WHERE name = null;"
 end
-
